fc532392e22b0b0350e4efb17bfc036a5a35b773
jleyver37 <90491236+jleyver37@users.noreply.github.com>
2021-09-24 22:22:47 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,6 +10,11 @@ basic.forever(function () {
         basic.showArrow(ArrowNames.NorthWest)
     }
 })
+// ¿Qué pasa cuando presionas A y muy 
+// rápidamente B? 
+// Se alternan las direcciones de la flechas, se alternan los bloques de la condición A con los de la condición B
+// ¿Es lo que esperabas?
+// No, yo esperaba que la flecha girara en sentido contrario desde la última flecha presentada, no alternándose direcciones entre bloques.
 basic.forever(function () {
     while (input.buttonIsPressed(Button.B)) {
         basic.showArrow(ArrowNames.NorthWest)
